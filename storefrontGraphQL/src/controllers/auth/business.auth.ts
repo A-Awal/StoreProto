416276e2cb00e57aec37b950be8fdd41b2397077
A-Awal <abdulai.awal@amalitech.org>
2023-03-14 08:19:02 +0000

    first apollo server integration

--- storefrontGraphQL/src/controllers/auth/business.auth.ts
+++ storefrontGraphQL/src/controllers/auth/business.auth.ts
@@ -1,10 +1,11 @@
 import {Resolver, Query, Ctx} from 'type-graphql';
 import { Product } from '../../entities/product';
 import { ApiContext } from '../graphqlTypes/context';
+import { ProductDto } from '../graphqlTypes/productDto';
 
 @Resolver()
 export class BusinessRegistrationReolver{
-  @Query(() => [Product])
+  @Query(() => [ProductDto])
   async Hello(@Ctx() context: ApiContext)
   {
     return context.dataSources.productAPI.getProducts();
