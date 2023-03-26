140b14a4e2b3c7c48fc4c569999e2323e769f63d
A-Awal <abdulai.awal@amalitech.org>
2023-03-26 02:09:39 +0000

    feat: front end II

--- Api/Program.cs
+++ Api/Program.cs
@@ -19,8 +19,8 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 if (app.Environment.IsDevelopment())
 {
 }
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
